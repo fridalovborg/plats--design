7b2a3a5d88a5139a2986a2a081ae227c28c3198e
Frida Lövborg <>
2018-02-12 16:11:41 +0100

    update color a kontakt

--- templates/head.php
+++ templates/head.php
@@ -1,5 +1,8 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
+	<meta name="description" content="Plats! är en designbyrå som hjälper små och stora företag att förpacka upplevelser inom t.ex. butik, restaurang och besöksnäring."/>
+	<meta name="robots" content="index, follow"/>
+	<meta name="googlebot" content="index, follow" />
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<?php wp_head(); ?>
